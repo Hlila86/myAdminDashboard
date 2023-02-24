097dcb67f428f3140bdb1ea6b1601a97ff5a6e04
hlila86 <hamdianoo@gmail.com>
2023-02-24 12:18:57 +0100

    setting navbar section

--- componements/Navbar.js
+++ componements/Navbar.js
@@ -1,9 +1,27 @@
+import Image from 'next/image'
 import React from 'react'
 import styles from '../styles/Navbar.module.css'
 
 const Navbar = () => {
   return (
-    <div className={styles.container}>Navbar</div>
+    <div className={styles.container}>
+
+        <div className={styles.logoContainer}>
+              <Image src='/img/logo.png' alt='' width={200} height={70}/>
+        </div>
+
+        <div className={styles.searchSection}>
+
+            <input type='search'></input>
+
+        </div>
+
+        <div className={styles.right}>
+
+            <h3>login</h3>
+
+        </div>
+    </div>
   )
 }
 
